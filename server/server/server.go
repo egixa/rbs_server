f49830e6a58b6a9e887618d99a5501e92d3a2b10
egixa <irona114@yandex.ru>
2024-07-05 13:47:51 +0400

    Работает!!

--- server/server/server.go
+++ server/server/server.go
@@ -1,140 +1,138 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	Filesistem "svr/filesystem"
-	"syscall"
-	"time"
+ "context"
+ "encoding/json"
+ "fmt"
+ "log"
+ "net/http"
+ "os"
+ "os/signal"
+ Filesistem "svr/filesystem"
+ "syscall"
+ "time"
 )
 
 // Config - структура для хранения конфигурации
 type Config struct {
-	Port int `json:"port"`
+ Port int json:"port"
 }
 
 // validateArgs проверяет валидные или невалидные флаги
 func validateArgs(rootFolder string, sortOption string) (string, error) {
-	// Валидация флагов
-	if rootFolder == "" {
-		return "", fmt.Errorf("Отсутствуют данные о местоположении директории")
-	}
-	if sortOption != Filesistem.Asc && sortOption != Filesistem.Desc {
-		sortOption = Filesistem.Asc
-		fmt.Println("Введен некорректный параметр сортировки. По умолчанию будет использована сортировка по возрастанию.")
-		return sortOption, nil
-	}
+ // Валидация флагов
+ if rootFolder == "" {
+  return "", fmt.Errorf("Отсутствуют данные о местоположении директории")
+ }
+ if sortOption != Filesistem.Asc && sortOption != Filesistem.Desc {
+  sortOption = Filesistem.Asc
+  fmt.Println("Введен некорректный параметр сортировки. По умолчанию будет использована сортировка по возрастанию.")
+  return sortOption, nil
+ }
 
-	// Проверка существования директории
-	_, err := os.Stat(rootFolder)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return "", fmt.Errorf("Такой директории не существует", err)
-		} else {
-			return "", fmt.Errorf("Ошибка при обнаружении директории", err)
-		}
-	}
-	return sortOption, nil
+ // Проверка существования директории
+ _, err := os.Stat(rootFolder)
+ if err != nil {
+  if os.IsNotExist(err) {
+   return "", fmt.Errorf("Такой директории не существует", err)
+  } else {
+   return "", fmt.Errorf("Ошибка при обнаружении директории", err)
+  }
+ }
+ return sortOption, nil
 }
-
-//const p = "/"
 
 // handleRequest принимает ответ от сервера и отправляет отсортированный массив с информацией о содержимом
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+ w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	// Разбираем URL-адрес и извлекаем флаги
-	query := r.URL.Query()
+ // Разбираем URL-адрес и извлекаем флаги
+ query := r.URL.Query()
 
-	rootFolder := query.Get("root")
-	sortOption := query.Get("sort")
+ rootFolder := query.Get("root")
+ sortOption := query.Get("sort")
 
-	// Проверяем валидность флагов
-	sortOption, err := validateArgs(rootFolder, sortOption)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+ // Проверяем валидность флагов
+ sortOption, err := validateArgs(rootFolder, sortOption)
+ if err != nil {
+  fmt.Println(err)
+  return
+ }
 
-	// Получаем отсортированное содержимое директории
-	files, err := Filesistem.GetFolder(rootFolder, sortOption)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+ // Получаем отсортированное содержимое директории
+ files, err := Filesistem.GetFolder(rootFolder, sortOption)
+ if err != nil {
+  fmt.Println(err)
+  return
+ }
 
-	// Сериализуем данные
-	jsonBytes, err := json.Marshal(files)
-	if err != nil {
-		http.Error(w, "Ошибка при преобразовании в json", http.StatusNotFound)
-	}
-	
-	w.Write(jsonBytes)
-	w.Header().Set("Content-Type", "application/json")
-	
+ // Сериализуем данные
+ jsonBytes, err := json.Marshal(files)
+ if err != nil {
+  http.Error(w, "Ошибка при преобразовании в json", http.StatusNotFound)
+ }
+ 
+ w.Write(jsonBytes)
+ w.Header().Set("Content-Type", "application/json")
+ 
 
 
-	/*// Отправляем данные на сервер
-	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(jsonBytes)
-	if err != nil {
-	 log.Printf("error while writing: %v", err)
-	 return
-	}*/
+ /*// Отправляем данные на сервер
+ w.Header().Set("Content-Type", "application/json")
+ _, err = w.Write(jsonBytes)
+ if err != nil {
+  log.Printf("error while writing: %v", err)
+  return
+ }*/
 
-	//fmt.Fprintf(w, string(jsonBytes))
+ //fmt.Fprintf(w, string(jsonBytes))
 
 }
 
 func main() {
 
-	// Читаем конфигурацию из файла
-	file, err := os.Open("./config/config.json")
-	if err != nil {
-		log.Fatalf("Ошибка открытия конфигурации: %v", err)
-	}
-	defer file.Close()
+ // Читаем конфигурацию из файла
+ file, err := os.Open("./config/config.json")
+ if err != nil {
+  log.Fatalf("Ошибка открытия конфигурации: %v", err)
+ }
+ defer file.Close()
 
-	var Config Config
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(&Config); err != nil {
-		log.Fatalf("Ошибка разбора JSON: %v", err)
-	}
+ var Config Config
+ decoder := json.NewDecoder(file)
+ if err := decoder.Decode(&Config); err != nil {
+  log.Fatalf("Ошибка разбора JSON: %v", err)
+ }
 
-	// Устанавливаем роутер
-	http.HandleFunc("/", handleRequest)
+ // Устанавливаем роутер
+ http.HandleFunc("/", handleRequest)
 
-	// Создаем сервер
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", Config.Port),
-		Handler: http.HandlerFunc(handleRequest),
-	}
+ // Создаем сервер
+ srv := &http.Server{
+  Addr:    fmt.Sprintf(":%d", Config.Port),
+  Handler: http.HandlerFunc(handleRequest),
+ }
 
-	// Запускаем сервер в отдельной горутине
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("ListenAndServe: %v", err)
-		}
-	}()
+ // Запускаем сервер в отдельной горутине
+ go func() {
+  if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+   log.Fatalf("ListenAndServe: %v", err)
+  }
+ }()
 
-	// Создаем канал для сигналов
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+ // Создаем канал для сигналов
+ interrupt := make(chan os.Signal, 1)
+ signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
-	// Ожидаем сигнал прерывания
-	<-interrupt
+ // Ожидаем сигнал прерывания
+ <-interrupt
 
-	// Даем серверу 5 секунд на завершение работы
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+ // Даем серверу 5 секунд на завершение работы
+ ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+ defer cancel()
 
-	// Завершаем работу сервера
-	srv.Shutdown(ctx)
-	fmt.Println("Сервер остановлен")
+ // Завершаем работу сервера
+ srv.Shutdown(ctx)
+ fmt.Println("Сервер остановлен")
 
 }
--- view/js.js
+++ view/js.js
@@ -1,19 +1,45 @@
 const url = "http://localhost:3007/";
-const path = document.querySelector(".path").innerHTML;
+const Asc = "asc"
+const Desc = "desc"
 
+function splitPath(path){
+    const parts = path.split('/');
+    parts.pop();
+    path = parts.join('/');
+    return path
+}
+//GET-запрос на получение содержимого директории
 async function getTableFolder(link){
-    response = await fetch(url + "?root=" + path + link.textContent + "&sort=" + sortButton.textContent, { // Запрос на сервер
+    path = document.querySelector('.path').innerHTML
+    var linkContent;
+    if (link === "Назад") {
+        path = splitPath(path);
+        linkContent = "";
+    }
+    else {
+        linkContent = link.textContent;
+        // if (path === "/") {
+        //     path += link.textContent;
+        // } else {
+        //     path += "/" + link.textContent;
+        // }
+    }
+
+    response = await fetch(url + "?root=" + path + "/" + linkContent + "&sort=" + sortButton.textContent, { // Запрос на сервер
         method: 'GET',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded' 
         }
-    });
-    response = await response.json();
-    fileTableBody.innerHTML = '';
+    }).then(response => response.json()).catch();
     return response
 }
 
+// Создание таблицы с данными из GET-запроса
 function createTable(data){
+    path = document.querySelector('.path').innerHTML
+
+    // Очищаем таблицу
+    fileTableBody.innerHTML = '';
      // Заполняем таблицу данными
      data.forEach(item => {
         const row = fileTableBody.insertRow();
@@ -27,14 +53,14 @@ function createTable(data){
             link.href = "#"; // Замените на реальный URL, если необходимо
             link.textContent = item.name;
             link.addEventListener('click', async (event) => {
+                clickGet(link)
+                
                 if (path === "/"){
-                    path = path + link.textContent
-                    console.log()
+                    document.querySelector('.path').innerHTML = path + link.textContent;
                 }
                 else{
-                    path = path + "/" + link.textContent
+                    document.querySelector('.path').innerHTML = path + "/" + link.textContent;
                 }
-                clickGet(link)
                 });
             nameCell.appendChild(link);
         } else {
@@ -45,57 +71,49 @@ function createTable(data){
       });
 }
 
+// Нажатие на ссылку в директорию
 async function clickGet(link){
     data = await getTableFolder(link);
-
     createTable(data);
 }
 
+// Кнопка сортировки
 const sortButton = document.querySelector('.sort-button');
-    let sortAscending = false;
-    let AskDesk = "asc";
+let sortAscending = false;
+let AskDesk = Asc;   
 
-    sortButton.addEventListener('click', async() => {
+//Обработка нажатия на кнопку сортировки
+sortButton.addEventListener('click', async() => {
     sortAscending = !sortAscending;
-    sortButton.textContent = sortAscending ? 'asc' : 'desc' ;
+    sortButton.textContent = sortAscending ? Asc : Desc ;
     if (sortAscending == true){
-        sortButton.textContent =  "asc";
+        sortButton.textContent =  Asc;
     }
-    else {sortButton.textContent = "desc"};
+    else {sortButton.textContent = Desc};
     
-    const response = await fetch(url + "?root=" + path + "&sort=" + sortButton.textContent, { // Запрос на сервер
+    path = document.querySelector('.path').innerHTML
+
+    const response = await fetch(url + "?root=" + path + "&sort=" + sortButton.textContent, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded' 
         }
     });
-    
     const data = await response.json(); // Получаем JSON-данные
-    
-      // Очищаем таблицу
-      fileTableBody.innerHTML = '';
-    
-      // Заполняем таблицу данными
-      data.forEach(item => {
-        const row = fileTableBody.insertRow();
-        const typeCell = row.insertCell();
-        const nameCell = row.insertCell();
-        const sizeCell = row.insertCell();
-
-        // Добавляем ссылку для папок
-        if (item.type === "Директория") {
-            const link = document.createElement('a');
-            link.href = "#"; // Замените на реальный URL, если необходимо
-            link.textContent = item.name;
-            link.addEventListener('click', async (event) => {
-                clickGet(link)
-            });
-            nameCell.appendChild(link);
-        }
-        else {
-            nameCell.textContent = item.name;
-          }
-          typeCell.textContent = item.type;
-          sizeCell.textContent = item.formatingSize;
-      })
+    createTable(data);
     })
+
+    //Обратоботка нажатия на кнопку Назад
+const backButton = document.querySelector('.back-button');
+backButton.addEventListener('click', async () => {
+    const link = "Назад";
+    clickGet(link);
+
+    path = document.querySelector('.path').innerHTML
+    if (path === "/"){
+        //
+    }
+    else{
+        document.querySelector('.path').innerHTML = splitPath(path);
+    }
+})
